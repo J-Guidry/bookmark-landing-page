d58452206b30061701e22be36c53fb7040e84999
Guidry <jh_guidry@outlook.com>
2019-11-05 16:33:20 -0600

    keyboard and click tab system created with javascript with accessibility

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const navButton = document.querySelector(".mobile-nav-icon");
 const body = document.querySelector("body");
 const logo = document.querySelector("#logo");
 const header = document.querySelector("header");
+const tablinks = document.querySelector(".tab-links");
+const tabs = tablinks.querySelectorAll("a");
+const panels = document.querySelectorAll(".panel");
 
 function toggleMobileNav() {
     navMenu.classList.toggle("is-active");
@@ -31,4 +34,49 @@ function toggleMobileNav() {
     }
  }
 
- navButton.addEventListener("click", toggleMobileNav);
+navButton.addEventListener("click", toggleMobileNav);
+
+// activate panels and change aria attributes when switching tabs
+function switchTab(oldTab, chosenTab) {
+    chosenTab.focus();
+    chosenTab.setAttribute("aria-selected", "true");
+    chosenTab.removeAttribute("tabindex");
+    chosenTab.classList.toggle("active");
+    oldTab.setAttribute("aria-selected", 'false');
+    oldTab.setAttribute("tabindex", "-1");
+    oldTab.classList.toggle("active");
+    let index = Array.prototype.indexOf.call(tabs,chosenTab);
+    let oldIndex = Array.prototype.indexOf.call(tabs,oldTab);
+    panels[oldIndex].classList.toggle("active");
+    panels[oldIndex].setAttribute("aria-hidden", "true");
+    panels[index].classList.toggle("active");
+    panels[index].setAttribute("aria-hidden", "false");
+}
+
+tabs.forEach(function(tab, i) {
+
+    tab.addEventListener("click", function(event) {
+        event.preventDefault();
+        const currentlySelectedTab = document.querySelector('[aria-selected="true"]');
+        const newlySelectedTab = event.currentTarget;
+        if (currentlySelectedTab !== newlySelectedTab) {
+            switchTab(currentlySelectedTab, newlySelectedTab);
+        }
+    });
+    
+    tab.addEventListener('keydown', e => {  
+        let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+        let dir = e.which === 37 ? index - 1 
+                : e.which === 39 ? index + 1 
+                : e.which === 40 ? 'down' : null;
+                
+        if (dir !== null) {
+            e.preventDefault();
+            dir === 'down' ? panels[i].focus() 
+                : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) 
+                : void 0;
+        }
+    });
+
+});
+
